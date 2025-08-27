d70a85c5b4ab5f22c710e49422e6c74f678794f3
Eng-Ayman-Twfaq <ayman.tawfaq.developers@gmail.com>
2025-08-27 13:15:26 -0700

    first commit

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://shadow-n7a6.onrender.com'),
 
     /*
     |--------------------------------------------------------------------------
